c2eb50615f3efeded21e30b7c615f826a7d0494d
Jonas Kruckenberg <jonas@crabnebula.dev>
2023-05-13 12:46:11 +0200

    feature parity

---
+++ checkpoints/3/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll(["/"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    console.log(event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
